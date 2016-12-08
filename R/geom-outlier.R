4268c318c1c2b8c9044ee4c0bfdfc1d1215faedc
dominikpeter <dominik.peter87@gmail.com>
2016-12-08 20:56:00 +0100

    minor

--- R/geom-outlier.R
+++ R/geom-outlier.R
@@ -16,14 +16,13 @@ require(ggplot2)
 get_outlier <- function(x, y = NULL) {
   stopifnot(is.vector(x) && (is.vector(y) || is.null(y)))
 
-  X <- cbind(x = x, y = y)
-
   get_idx_outlier <- function(x) {
     coef <- IQR(x, na.rm = TRUE)*1.5
     upper <- quantile(x, .75, names = FALSE) + coef
     lower <- quantile(x, .25, names = FALSE) - coef
     which(x > upper | x < lower)
   }
+  X <- cbind(x, y)
 
   idx <- unlist(apply(X, MARGIN = 2, get_idx_outlier))
   unique(idx)
