8c0f9c96d3950bbaac7fd54252d9b80e05384d24
dominikpeter <dominik.peter87@gmail.com>
2016-12-09 20:18:16 +0100

    new

--- R/get-outlier.R
+++ R/get-outlier.R
@@ -16,12 +16,6 @@
 get_outlier <- function(x, y = NULL) {
   stopifnot(is.vector(x) && (is.vector(y) || is.null(y)))
 
-  get_idx_outlier <- function(x) {
-    coef <- IQR(x, na.rm = TRUE)*1.5
-    upper <- quantile(x, .75, names = FALSE) + coef
-    lower <- quantile(x, .25, names = FALSE) - coef
-    which(x > upper | x < lower)
-  }
   X <- cbind(x, y)
 
   idx <- apply(X, MARGIN = 2, get_idx_outlier)
@@ -31,3 +25,13 @@ get_outlier <- function(x, y = NULL) {
 
   unique(idx)
 }
+
+
+
+get_idx_outlier <- function(x) {
+  IQR <- 1.5*IQR(x)
+  upper <- quantile(x, .75, names = FALSE) + IQR
+  lower <- quantile(x, .25, names = FALSE) - IQR
+  which(x > upper | x < lower)
+}
+
