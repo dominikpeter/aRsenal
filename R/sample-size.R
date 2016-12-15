9f398f3d26078a7a13031f2e5c3dcdaf465a6a60
dominikpeter <dominik.peter87@gmail.com>
2016-12-15 09:12:07 +0100

    bug

--- R/sample-size.R
+++ R/sample-size.R
@@ -14,10 +14,14 @@
 #'
 #'
 #' @export
-sample_size <- function(x, Margin.of.Error, conf.level = 0.95) {
+sample_size <- function(x, Margin.of.Error, conf.level = 0.95, ...) {
   conf <- 1-((1-conf.level)/2)
   zstar <- qnorm(conf)
-
+  sigma <- sd(x, ...)
   zstar^2 * sigma^2 / Margin.of.Error^2
-
 }
+
+
+
+
+
