81b1293ec4f22c2453d67aae3525c9af93b7ba88
dominikpeter <dominik.peter87@gmail.com>
2016-12-15 15:02:01 +0100

    minor

--- R/get-outlier.R
+++ R/get-outlier.R
@@ -13,21 +13,10 @@
 #'mtcars[get_outlier(mtcars$mpg, mtcars$hp), ]
 #'
 #' @export
-# get_outlier <- function(x, y = NULL) {
-#   stopifnot(is.numeric(x) && (is.numeric(y) || is.null(y)))
-#
-#   X <- cbind(x, y)
-#
-#   idx <- apply(X, MARGIN = 2, get_idx_outlier)
-#
-#   if (is.list(idx))
-#     idx <- unlist(idx)
-#
-#   unique(idx)
-# }
-
 get_outlier <- function(x, y = NULL) {
-  if (missing(x) && is.numeric(x) && (is.numeric))
+  if (missing(x) ||
+      !is.numeric(x) ||
+      (!is.numeric(y) && !is.null(y)))
     stop("You must provide a x value and it has to be numeric")
 
   X <- as.data.frame(cbind(x = x, y = y))
@@ -41,27 +30,10 @@ get_outlier <- function(x, y = NULL) {
   } else {
     mod <- lm(X$y ~ X$x, data = X)
     cooksd <- unname(cooks.distance(mod))
-    upper <- 4*mean(cooksd, na.rm=T)
+    upper <- 4*mean(cooksd, na.rm=TRUE)
     idx <- which(cooksd > upper)
   }
   idx
 }
-
-# get_idx_outlier <- function(x) {
-#   IQR <- 1.5*IQR(x)
-#   upper <- quantile(x, .75, names = FALSE) + IQR
-#   lower <- quantile(x, .25, names = FALSE) - IQR
-#   which(x > upper | x < lower)
-# }
-
-# get_idx_outlier <- function(x) {
-#   IQR <- 1.5*IQR(x)
-#   tails <- c(upper = .75, lower = .25)
-#   tails[] <- quantile(x, tails, names = FALSE) + c(IQR, -IQR)
-#   which(x > tails["upper"] | x < tails["lower"])
-# }
-
-
-
 
 
